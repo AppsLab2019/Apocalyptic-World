72be116bd424645a9ba5aa160b46adbbf261ce30
samuel zaťovič <szatovic@gmail.com>
2020-02-22 15:23:17 +0100

    Add project files.

---
+++ ApocalypticWorld/ApocalypticWorld/Page2.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ApocalypticWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page3());
+        }
+    }
+}
